3ce2f7b453cf378c7f36e78bb1f877ce238e5c11
Yoroitchi <r1leguern@hotmail.fr>
2020-02-04 10:42:26 +0100

    Removing all about Issuer role for credentials

--- AriesWebApp/Controllers/CredentialsController.cs
+++ AriesWebApp/Controllers/CredentialsController.cs
@@ -49,18 +49,6 @@ namespace AriesWebApp.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> ProcessRequest(string id)
-        {
-            var agentContext = await _agentContextProvider.GetContextAsync();
-            var credentialRecord = await _credentialService.GetAsync(agentContext, id);
-            var connectionRecord = await _connectionService.GetAsync(agentContext, credentialRecord.ConnectionId);
-            (var cred, _) = await _credentialService.CreateCredentialAsync(agentContext: agentContext, credentialId: id);
-            await _messageService.SendAsync(agentContext.Wallet, cred, connectionRecord);
-            
-            return RedirectToAction("Index");
-        }
-
-        [HttpGet]
         public async Task<IActionResult> ProcessOffer(string id)
         {
             //id is the credential Record Id
@@ -98,57 +86,6 @@ namespace AriesWebApp.Controllers
             var agentContext = await _agentContextProvider.GetContextAsync();
             var credentialRecord = await _credentialService.GetAsync(agentContext, credRecId);
             return View(credentialRecord);
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> SendOfferCredential(string connectionId, string credDefId)
-        {
-            var agentContext = await _agentContextProvider.GetContextAsync();
-            var connectionRecord = await _connectionService.GetAsync(agentContext, connectionId);
-
-            //{ "type", "passportNumber", "issuerCountryCode", "firstname", "familyname", "birthdate", "citizenship", "sex", "placeOfBirth", "issuingDate", "expiryDate" }
-             var offerConfig = new OfferConfiguration
-             {
-                 CredentialDefinitionId = credDefId,
-                 IssuerDid = "Th7MpTaRZVRYnPiabds81Y",
-                 CredentialAttributeValues = new [] 
-                 { 
-                     new CredentialPreviewAttribute("type","passport"), 
-                     new CredentialPreviewAttribute("passportNumber", $"{Guid.NewGuid().ToString("N")}"),
-                     new CredentialPreviewAttribute("issuerCountryCode", "CH"),
-                     new CredentialPreviewAttribute("firstname", "John"),
-                     new CredentialPreviewAttribute("familyname","Doe"),
-                     new CredentialPreviewAttribute("birthdate","1968-02-12T:15:00:00Z"),
-                     new CredentialPreviewAttribute("citizenship","CH"),
-                     new CredentialPreviewAttribute("sex","M"),
-                     new CredentialPreviewAttribute("placeOfBirth", "Paris"),
-                     new CredentialPreviewAttribute("issuingDate", "20-01-2020"),
-                     new CredentialPreviewAttribute("expiryDate", "20-01-2030")
-                 }
-             };
-            /*var offerConfig = new OfferConfiguration
-            {
-                CredentialDefinitionId = credDefId,
-                IssuerDid = "Th7MpTaRZVRYnPiabds81Y",
-                CredentialAttributeValues = new[] { new CredentialPreviewAttribute("first_name", "Test"), new CredentialPreviewAttribute("last_name", "Holder") }
-            };*/
-
-            (var credOfferMsg, _) = await _credentialService.CreateOfferAsync(agentContext, offerConfig, connectionId);
-            await _messageService.SendAsync(agentContext.Wallet, credOfferMsg, connectionRecord);
-
-            return RedirectToAction("Details", "Connections", new { id = connectionId });
-        }
-
-        [HttpGet]
-        public async Task<IActionResult> CreateOfferFromSchemaId(string schemaId, string connectionId)
-        {
-            var agentContext = await _agentContextProvider.GetContextAsync();
-            var schemaRecord = await _walletRecordService.GetAsync<SchemaRecord>(agentContext.Wallet, schemaId);
-            return View(new OfferViewModel
-            {
-                Schema = schemaRecord,
-                ConnectionId = connectionId
-            });
         }
 
     }
